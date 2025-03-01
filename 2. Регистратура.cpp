645f525e5eba9c8824daf79dc4b1630560f7b161
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-03-01 15:03:55 +0300

    Add files via upload

---
+++ 2. Регистратура.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+int main() {
+    std::map<std::string, int> queue;
+
+    std::string input;
+    while (std::cin >> input) {
+        if (input == "Next") {
+            // Найти первую фамилию в отсортированной очереди
+            auto it = queue.begin();
+            if (it != queue.end()) {
+                std::cout << it->first << "\n";
+
+                // Уменьшить счетчик на единицу
+                if (--queue[it->first] == 0) {
+                    // Если больше нет людей с такой фамилией, удаляем её из очереди
+                    queue.erase(it);
+                }
+            }
+        }
+        else if (input == "q") {
+            break;
+        }
+        else {
+            // Добавляем пациента в очередь
+            ++queue[input];
+        }
+    }
+}
